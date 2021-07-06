e7f4faec2ca99128d19951e26e6ba979e054805a
Sluchevska <alaska3a@gmail.com>
2021-07-06 20:33:15 +0100

    update task 1

--- src/task1.js
+++ src/task1.js
@@ -1,8 +1,8 @@
 const delay = ms => {
   // Твой код
-    return new Promise((resolved) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
-          
+       resolve
         }, delay)
     })
 };
